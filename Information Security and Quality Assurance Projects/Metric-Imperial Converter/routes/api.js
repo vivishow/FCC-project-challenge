a7d5730da621b3f48e8defa4e5871291841a03f2
vivishow <78064059@qq.com>
2019-06-09 17:40:31 +0800

    add ConvertHandler

--- Information Security and Quality Assurance Projects/Metric-Imperial Converter/routes/api.js
+++ Information Security and Quality Assurance Projects/Metric-Imperial Converter/routes/api.js
@@ -9,9 +9,11 @@
 'use strict';
 
 var expect = require('chai').expect;
+var ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
   
+  var convertHandler = new ConvertHandler();
   app.route('/api/convert')
     .get(function (req, res){
       var input = req.query.input;
